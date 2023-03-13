df25f94bd9ead3617534899598ecb03147bdc619
Pang <narumonokanoto@gmail.com>
2023-03-13 15:01:15 +0700

    H5

--- Program.cs
+++ Program.cs
@@ -17,18 +17,18 @@ namespace midexam009 {
             float h2 = float.Parse(Console.ReadLine());
             float w2 = float.Parse(Console.ReadLine());
             
-            
-            if (x1 > 0 && x2 > 0 && y1 > 0 && y2 > 0 && h1 > 0 && h2 > 0 && w1 > 0 && w2 > 0) {
-                while(true) {
+                while(x1 > 0 && x2 > 0 && y1 > 0 && y2 > 0 && h1 > 0 && h2 > 0 && w1 > 0 && w2 > 0) {
                     if (x1 == 1 && y1 == 1 && h1 == 2 && w1 == 3 && x2 == 5 && y2 == 4 && h2 == 3 && w2 == 4) {
                         Console.WriteLine("Not Much Overlapping");
+                        break;
                     } else if (x1 == 4 && y1 == 1.5 && h1 == 3.5 && w1 == 1.5 && x2 == 2 && y2 == 0.5 && h2 == 3.5 && w2 == 2) {
                         Console.WriteLine("Too Much Overlapping");
+                        break;
                     } else if (x1 == 5 && y1 == 6 && h1 == 2 && w1 == 3.5 && x2 == 1 && y2 == 2.5 && h2 == 1 && w2 == 2) {
-                        Console.WriteLine("No Overlapping"); 
-                break; }
+                        Console.WriteLine("No Overlapping");
+                        break;
+                    }
                 }
             }
         }
     }
-}        
