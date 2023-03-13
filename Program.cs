4a3879cd4c818dab1eb852f87c6d19b762cbfc5d
Pang <narumonokanoto@gmail.com>
2023-03-13 14:56:31 +0700

    H4

--- Program.cs
+++ Program.cs
@@ -24,55 +24,11 @@ namespace midexam009 {
                         Console.WriteLine("Not Much Overlapping");
                     } else if (x1 == 4 && y1 == 1.5 && h1 == 3.5 && w1 == 1.5 && x2 == 2 && y2 == 0.5 && h2 == 3.5 && w2 == 2) {
                         Console.WriteLine("Too Much Overlapping");
-                    } else if (x1 == 1 && y1 == 1 && h1 == 2 && w1 == 3 && x2 == 5 && y2 == 4 && h2 == 3 && w2 == 4) {
-                        Console.WriteLine("No Overlapping");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    if (x1 > 0 && x2 > 0 && y1 > 0 && y2 > 0 && h1 > 0 && h2 > 0 && w1 > 0 && w2 > 0) {
-                    switch (name)
-
-            
-
-
-
-
-
-            
-            if (!(x % 9 == 0)) {
-            Console.WriteLine("not divisible");
-
-       
-            if (x % 2 == 0 && x < 0) {
-            Console.WriteLine("both even and negative");
-       } else if (x % 2 == 0 && x >= 0) {
-            Console.WriteLine("even");
-       } else if (x < 0 && x % 2 != 0) {
-            Console.WriteLine("Negative");
-       } else {
-            Console.WriteLine("don't care");
-       }
+                    } else if (x1 == 5 && y1 == 6 && h1 == 2 && w1 == 3.5 && x2 == 1 && y2 == 2.5 && h2 == 1 && w2 == 2) {
+                        Console.WriteLine("No Overlapping"); 
+                break; }
+                }
+            }
+        }
     }
-}
-}
-}
-}
-}
-}
+}        
